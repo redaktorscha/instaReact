61cdfa729d16a3283739b60556bcfb7a84078f4a
Masha <nasonkina@yandex.ru>
2019-10-31 02:54:26 +0300

    posts props

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 
 //post for Posts
 export default class Post extends Component {
+    
     render() {
+        const { src, alt } = this.props;
         return (
-            <div className='post'>
+            <div className='post'>                
 
-                <img src={this.props.src} alt={this.props.alt}></img>
+                <img src={src} alt={alt}></img>
                 <div className='post__name'>post name</div>
                 <div className='post__descr'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
             </div>
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -5,7 +5,8 @@ export default class Posts extends Component {
     render() {
         return (
             <div className='left'>
-                <Post/>
+                <Post src='https://static.boredpanda.com/blog/wp-content/uploads/2018/10/The-Australian-Firefighters-2019-calendar-has-already-been-announced-and-this-charity-is-very-beautiful-to-see-5bbf039e6b9ff__700.jpg' alt='first'/>
+                <Post src='https://hips.hearstapps.com/ghk.h-cdn.co/assets/17/42/1024x1375/gallery-1508259728-hot-firefighters-australian-firefighters-calendar-2018122-6.jpg' alt='second'/>
             </div>
         )
     }
