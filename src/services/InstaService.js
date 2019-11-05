d4cc49295d38e12e22847cfbec4b963d71cfbe28
Masha <nasonkina@yandex.ru>
2019-11-05 12:11:34 +0300

    minor fix in posts.js + package.json

--- src/services/InstaService.js
+++ src/services/InstaService.js
@@ -2,7 +2,7 @@
 //fetching data from server (async)
 export default class InstaService {
     constructor() {
-        this._apiBase = 'http://localhost:3000';
+        this._apiBase = 'http://localhost:3001';
     }
 
     //receive data
