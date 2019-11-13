266b4ea33ee23c24f6862c8ba4affa957e3d18f7
Masha <nasonkina@yandex.ru>
2019-11-13 15:47:04 +0300

    users component fixed

--- src/services/InstaService.js
+++ src/services/InstaService.js
@@ -12,33 +12,31 @@ export default class InstaService {
         if (!res.ok) {
             throw new Error(`Could not fetch ${url}, received ${res.status}`);
         };
-
         
-        let result = await res.json();
-        const {posts} = result;
-        console.log(posts.map(this._filterPosts));
-
+        //const result = await res.json();
+        //const {posts, users} = result;        
         return await res.json();
     }
 
     //get all posts from server
     getAllPosts = async () => {
-        //const res = await this.getResource('/posts/');
-        return await this.getResource('/posts/');
-        // console.log('getAllPosts', res);
+        const res = await this.getResource('/posts/');
+        const {posts} = res;
+        return await posts;
     }
 
 //data for right column
     getUsers = async () => {
-        return await this.getResource('/users/');
-        // console.log ('getUsers', res);
+        const res = await this.getResource('/users/');
+        const {users} = res;
+        return users;
     }
 
 //pics for palette
     getAllPhotos = async () => {
        const res = await this.getResource('/posts/');
-       console.log('getAllPhotos: ', res);
-       return res.map(this._filterPosts);
+       const {posts} = res;
+       return posts.map(this._filterPosts);
     }
 
     _filterPosts = (post) => {
